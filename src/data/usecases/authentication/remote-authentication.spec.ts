5244411fe1c1392bb61199651c20d05476d3cdc7
lucastak <contato.lucas.santos@hotmail.com>
2023-01-02 22:01:27 -0300

    fix: remove faker

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -6,7 +6,7 @@ type SutTypes = {
     httpPostClientSpy: HttpPostClienteSpy
 }
 
-const makeSut = (url : string = 'any_url'): SutTypes => {
+const makeSut = (url : string = 'aa'): SutTypes => {
     const httpPostClientSpy = new HttpPostClienteSpy()
     const sut = new RemoteAuthentication(url, httpPostClientSpy)
     return{
@@ -17,7 +17,7 @@ const makeSut = (url : string = 'any_url'): SutTypes => {
 
 describe('RemoteAuthentication', () => {
     test('Should call HttpPostClient with correct URL', async () => {
-        const url = 'other_url'
+        const url = 'bb'
         const {sut, httpPostClientSpy} = makeSut(url);
         await sut.auth()
         expect(httpPostClientSpy.url).toBe(url)
