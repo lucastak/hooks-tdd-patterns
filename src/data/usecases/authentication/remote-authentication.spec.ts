ac47ccefa4ffb18c1068e88e0da10a03d73b005f
lucastak <contato.lucas.santos@hotmail.com>
2023-01-02 22:04:56 -0300

    refactor: refatora teste do htppPostClient

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,12 +1,13 @@
 import { RemoteAuthentication } from "./remote-authentication"
 import { HttpPostClienteSpy } from "../../test/mock-http-client"
+import { faker } from '@faker-js/faker';
 
 type SutTypes = {
     sut: RemoteAuthentication
     httpPostClientSpy: HttpPostClienteSpy
 }
 
-const makeSut = (url : string = 'aa'): SutTypes => {
+const makeSut = (url : string = faker.internet.url()): SutTypes => {
     const httpPostClientSpy = new HttpPostClienteSpy()
     const sut = new RemoteAuthentication(url, httpPostClientSpy)
     return{
@@ -17,8 +18,8 @@ const makeSut = (url : string = 'aa'): SutTypes => {
 
 describe('RemoteAuthentication', () => {
     test('Should call HttpPostClient with correct URL', async () => {
-        const url = 'bb'
-        const {sut, httpPostClientSpy} = makeSut(url);
+        const url = faker.internet.url()
+        const {sut, httpPostClientSpy} = makeSut(url)
         await sut.auth()
         expect(httpPostClientSpy.url).toBe(url)
     })
