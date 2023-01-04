7abb97f751067d33303fe1c0a7a35fbb56039b67
lucastak <contato.lucas.santos@hotmail.com>
2023-01-03 21:32:25 -0300

    refactor: muda httppostcliente para receber objeto

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,10 +1,11 @@
 import { HttpPostClient } from "data/protocols/http/http-post-client"
+import { HttpPostParams } from "data/protocols/http/http-post-client"
 
 export class HttpPostClienteSpy implements HttpPostClient{
     url?: string
 
-    async post(url: string): Promise<void> {
-        this.url = url
+    async post(params: HttpPostParams): Promise<void> {
+        this.url = params.url
         return Promise.resolve()
     }
 
