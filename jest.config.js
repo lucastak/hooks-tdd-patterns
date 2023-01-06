17b3e478395dc53cce1a9b6c300a775457e93f69
lucastak <contato.lucas.santos@hotmail.com>
2023-01-06 15:32:30 -0300

    refactor: muda imports para to use module

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,7 @@ module.exports = {
     transform: {
       '.+\\.ts$': 'ts-jest'
     },
+    moduleNameMapper:{
+        '@/(.*)': '<rootDir>/src/$1'
+    }
 }
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,6 @@
 import { RemoteAuthentication } from "./remote-authentication"
-import { HttpPostClienteSpy } from "../../test/mock-http-client"
-import { mockAuthentication } from "../../../domain/test/mock-authentication";
+import { HttpPostClienteSpy } from "@/data/test/mock-http-client"
+import { mockAuthentication } from "@/domain/test/mock-authentication";
 import { faker } from '@faker-js/faker';
 
 type SutTypes = {
