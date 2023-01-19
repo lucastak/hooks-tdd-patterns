58b2d9e9223e2bc09d91641a8ebbca9a8b3ec02e
lucastak <contato.lucas.santos@hotmail.com>
2023-01-18 22:03:23 -0300

    refactor: move a criacao do sut para um metodo factor

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -5,10 +5,14 @@ import { faker } from "@faker-js/faker"
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
+const makeSut = (): AxiosHttpClient => {
+    return new AxiosHttpClient()
+}
+
 describe('AxiosHttpClient' ,() => {
     test('Shoud call axios with correct url', async () => {
         const url = faker.internet.url()
-        const sut = new AxiosHttpClient()
+        const sut = makeSut()
         sut.post({url})
         expect(mockedAxios).toHaveBeenCalledWith(url)
     })
