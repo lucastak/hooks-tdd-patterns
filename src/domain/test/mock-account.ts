45cee24bcd200294483f1441cfc80b76db720c04
lucastak <contato.lucas.santos@hotmail.com>
2023-01-18 22:25:44 -0300

    fix: ajusta faker random do mockAccountModel

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -8,5 +8,5 @@ export const mockAuthentication = (): AuthenticationParams => ({
 })
 
 export const mockAccountModel = (): AccountModel => ({
-    accessToken: faker.random.alphaNumeric()
+    accessToken: faker.datatype.uuid()
 })
