1468ab0980cf4d43740e4d46fd5821434874a49a
lucastak <contato.lucas.santos@hotmail.com>
2023-01-27 17:23:05 -0300

    feat: adiciona comeco da estrutura do login

---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,13 @@
+
+import React from "react"
+import styles from "./login-styles.scss"
+
+const Login:React.FC = () => {
+    return(
+        <div className={styles.login}>
+            <span>Hello</span>
+        </div>
+    )
+}
+
+export default Login
