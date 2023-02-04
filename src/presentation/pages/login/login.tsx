8c5de7fecb49c8da494028770655a4365677c12f
lucastak <contato.lucas.santos@hotmail.com>
2023-02-04 14:39:18 -0300

    feat: move input para uma pasta separada

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -4,6 +4,7 @@ import styles from "./login-styles.scss"
 import Spinner from "@/presentation/components/spinner/spinner"
 import LoginHeader from "@/presentation/components/login-header/login-header"
 import Footer from "@/presentation/components/footer/footer"
+import Input from "@/presentation/components/input/input"
 
 const Login:React.FC = () => {
     return(
@@ -11,14 +12,8 @@ const Login:React.FC = () => {
             <LoginHeader />
             <form className={styles.form}>
                 <h2>Login</h2>
-                <div className={styles.inputWrap}>
-                    <input type="email" name="email" placeholder="Digite seu e-mail " />
-                    <span className={styles.status}>🔴</span>
-                </div>
-                <div className={styles.inputWrap}>
-                    <input type="password" name="password" placeholder="Digite sua senha " />
-                    <span className={styles.status}>🔴</span>
-                </div>
+                <Input type="email" name="email" placeholder="Digite seu e-mail "  />
+                <Input type="password" name="password" placeholder="Digite sua senha" />
                 <button className={styles.submit} type="submit">Entrar</button>
                 <span className={styles.link}>Criar conta</span>
 
