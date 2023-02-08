c2ce46c34117b84385217cd013bbc41006f83362
lucastak <contato.lucas.santos@hotmail.com>
2023-02-08 11:13:53 -0300

    feat: garante que o login comece com o submit button desabilitado

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -23,7 +23,7 @@ const Login:React.FC = () => {
                     <h2>Login</h2>
                     <Input type="email" name="email" placeholder="Digite seu e-mail "  />
                     <Input type="password" name="password" placeholder="Digite sua senha" />
-                    <button className={styles.submit} type="submit">Entrar</button>
+                    <button data-testid="submit" disabled className={styles.submit} type="submit">Entrar</button>
                     <span className={styles.link}>Criar conta</span>
                     <FormStatus />
                 </form>
