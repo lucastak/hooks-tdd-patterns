e3ec0f3c5021b565475015f8e6e0de768d17a292
lucastak <contato.lucas.santos@hotmail.com>
2023-02-17 15:53:03 -0300

    feat: garante que o Login apresente error se o email for invalido

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -14,13 +14,16 @@ const Login:React.FC<Props> = ({validation}: Props) => {
         isLoading: false,
         email: '',
         password: '',
-        emailError: 'Campo Obrigatório',
+        emailError: '',
         passwordError: 'Campo Obrigatório',
         mainError: '',
     })
 
     useEffect(() => {
-        validation.validate('email', state.email)
+        setState({
+            ...state,
+            emailError: validation.validate('email', state.email)
+        })
     }, [state.email])
 
     useEffect(() => {
