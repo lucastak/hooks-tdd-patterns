8cc95e9c5a0645346c4a99a27b63320f8b9cfa04
lucastak <contato.lucas.santos@hotmail.com>
2023-02-24 15:46:32 -0300

    feat: garante que o Login renderize o spinner no click do submit

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -27,11 +27,16 @@ const Login:React.FC<Props> = ({validation}: Props) => {
         })
     }, [state.email, state.password])
 
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault()
+        setState({...state, isLoading: true})
+    }
+
     return(
         <div className={styles.login}>
             <LoginHeader />
             <Context.Provider value={{state, setState}}>
-                <form className={styles.form}>
+                <form className={styles.form} onSubmit={handleSubmit}>
                     <h2>Login</h2>
                     <Input  type="email" name="email" placeholder="Digite seu e-mail"  />
                     <Input type="password" name="password" placeholder="Digite sua senha" />
