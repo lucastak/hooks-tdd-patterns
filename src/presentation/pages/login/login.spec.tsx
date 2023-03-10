8d3785889f6d357fa06cd06b03cd00885dd3e38f
lucastak <contato.lucas.santos@hotmail.com>
2023-03-10 16:42:40 -0300

    test: previne que o login envie o form se tiver algo invalido

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -133,4 +133,13 @@ describe("", () => {
 
         expect(authenticationSpy.callsCount).toBe(1)
     })
+
+    test("Should call Authentication if form is invalid", () => {
+        const validationError = faker.random.words()
+        const {sut, authenticationSpy} = makeSut({validationError})
+        populateEmailField(sut)
+
+        fireEvent.submit(sut.getByTestId('form'))
+        expect(authenticationSpy.callsCount).toBe(0)
+    })
 })
