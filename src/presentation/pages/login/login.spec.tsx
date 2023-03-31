6c87d0f0067491b7c37d04c5707965397aa8c596
Lucas Santos <lucassantos@MacBook-Air-de-Lucas.local>
2023-03-31 16:26:40 -0300

    test: garante que o login navegue para a pagina de signup

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,4 +1,6 @@
 import React from "react"
+import {Router} from 'react-router'
+import {createMemoryHistory} from 'history'
 import { faker } from "@faker-js/faker"
 import 'jest-localstorage-mock'
 import {render, RenderResult, fireEvent, cleanup, waitFor} from "@testing-library/react"
@@ -15,11 +17,17 @@ type SutParams = {
     validationError: string
 }
 
+const history = createMemoryHistory()
 const makeSut = (params?: SutParams):SutTypes => {
     const validationStub = new ValidationStub()
     const authenticationSpy = new AuthenticationSpy()
     validationStub.errorMessage = params?.validationError
-    const sut = render(<Login validation={validationStub} authentication={authenticationSpy} />)
+    const sut = render(
+        <Router history={history}>
+              <Login validation={validationStub} authentication={authenticationSpy} />
+        </Router>
+
+    )
     return{
         sut,
         authenticationSpy
@@ -165,8 +173,17 @@ describe("", () => {
     test("Shloud add accessToken to localstorage on success", async () => {
         const {sut, authenticationSpy} = makeSut()
         simulateValidSubmit(sut)
-        await waitFor(() => sut.getAllByTestId('form'))
+        await waitFor(() => sut.getByTestId('form'))
         expect(localStorage.setItem).toHaveBeenCalledWith('accessToken', authenticationSpy.account.accessToken)
+    })
+
+    test("Shloud to go signuppage", async () => {
+        const {sut, } = makeSut()
+        const register = sut.getByTestId('signup')
+
+        fireEvent.click(register)
+        expect(history.length).toBe(2)
+        expect(history.location.pathname).toBe('/signup')
     })
 
 })
