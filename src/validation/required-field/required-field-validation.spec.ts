cd68c7e138d1af782ae77ff1083bb01318e8ee4a
lucastak <contato.lucas.santos@hotmail.com>
2023-05-19 16:03:44 -0300

    test: garante que o RequiredFieldValidation returne falsy se o field nao for vazio

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,5 +1,6 @@
 import { RequiredFieldError } from "@/validation/errors";
 import { RequiredFieldValidation } from "./required-field-validation";
+import { faker } from "@faker-js/faker";
 
 
 describe('RequiredFieldValidation', () => {
@@ -7,5 +8,11 @@ describe('RequiredFieldValidation', () => {
         const sut = new RequiredFieldValidation('email')
         const error = sut.validate('')
         expect(error).toEqual(new RequiredFieldError())
+    });
+
+    test('Should return error if field is not empty ', () => {
+        const sut = new RequiredFieldValidation('email')
+        const error = sut.validate(faker.random.word())
+        expect(error).toBeFalsy();
     });
 })
