fc5f42961937f39e44270bc0b32a304bd6469399
lucastak <contato.lucas.santos@hotmail.com>
2023-06-09 16:57:41 -0300

    test: garante que o MinLengthValidation retorne falsy se o campo for valido

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -7,4 +7,10 @@ describe('MinLength validation', () => {
         const error = sut.validate(`123`)
         expect(error).toEqual(new InvalidFieldError())
     });
+
+    test('should return falsy if valid is valid', () => {
+        const sut = new NewLengthValidation('field', 5)
+        const error = sut.validate(`12345`)
+        expect(error).toBeFalsy()
+    });
 });
