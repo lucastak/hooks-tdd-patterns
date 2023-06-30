44025f3094bb3f49c4ee015c72364127e21f3b41
lucastak <contato.lucas.santos@hotmail.com>
2023-06-30 14:49:45 -0300

    refactor: move a criacao do sut para um metodo factory

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,16 +1,21 @@
 import { InvalidFieldError } from "@/validation/errors/invalid-field-error";
-import NewLengthValidation from "./min-length-validation";
+import MinLengthValidation from "./min-length-validation";
+
+import { faker } from "@faker-js/faker";
+
+
+const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.database.column(), 5)
 
 describe('MinLength validation', () => {
     test('should return error if valid is invalid', () => {
-        const sut = new NewLengthValidation('field', 5)
-        const error = sut.validate(`123`)
+        const sut = makeSut()
+        const error = sut.validate(faker.random.alphaNumeric(4))
         expect(error).toEqual(new InvalidFieldError())
     });
 
     test('should return falsy if valid is valid', () => {
-        const sut = new NewLengthValidation('field', 5)
-        const error = sut.validate(`12345`)
+        const sut = makeSut()
+        const error = sut.validate(faker.random.alphaNumeric(5))
         expect(error).toBeFalsy()
     });
 });
--- src/validation/validators/min-length/min-length-validation.ts
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -1,7 +1,7 @@
 import { InvalidFieldError } from "@/validation/errors/invalid-field-error";
 import { FieldValidation } from "@/validation/protocols/field-validation";
 
-export default class NewLengthValidation implements FieldValidation {
+export default class MinLengthValidation implements FieldValidation {
     constructor(readonly field: string, private readonly minLength: number){}
 
     validate(value: string): Error {
